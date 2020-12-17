3e7cb0eab6a2ef72c098fde7533c4327d82b7a13
juninhoww2 <alqadianoKira@gmail.com>
2020-12-16 22:53:40 -0300

    addfeatures

--- capacitorCourse/src/app/home/home.page.ts
+++ capacitorCourse/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public loadingController: LoadingController) {}
 
-}
+  async presentLoading() {
+      const loading = await this.loadingController.create({
+        cssClass: 'my-custom-class',
+        message: 'Please wait...',
+        duration: 2000
+      });
+      await loading.present();
+      
+      const { role, data } = await loading.onDidDimiss();
+        console.log('Loading dimissed!');
+      }
+
+      async presentLoadingWithOptions() {
+        const loading = await this.loadingController.create({
+          spinner: null,
+          duration: 5000,
+          message: 'Click the backdrop to dimiss early...',
+          translucent: true,
+          cssClass: 'custom-class custom-loading',
+          backdropDimiss: true
+        });
+
+        await loading.present();
+
+        const { role, data } = await loading.onDidDimiss();
+        console.log('Loading dimissed with role:', role);
+      }
+  }
